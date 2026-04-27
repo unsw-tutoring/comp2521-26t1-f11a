1fa13fdb7a1c8b93402a0db3f1e0a620d673d5df
Josh Lim <joshconstantinelim@gmail.com>
2026-04-27 15:21:48 +1000

    wk8

--- wk8/reachable.c
+++ wk8/reachable.c
@@ -5,7 +5,38 @@
 #include "Graph.h"
 #include "Set.h"
 
+/**
+ 
+dfs:
+    initialise stack and visited array
+
+    push src onto the stack
+
+    while stack is not empty:
+        pop v off the stack
+
+        push all of v's unvisited neighbours
+ */
+
+void dfs(Graph g, int v, bool *visited) {
+    visited[v] = true;
+
+    for (int w = 0; w < GraphNumVertices(g); w++) {
+        if (GraphIsAdjacent(g, v, w) && !visited[w]) {
+            dfs(g, w, visited);
+        }
+    }
+}
+
 Set reachable(Graph g, Vertex src) {
-    // TODO
-    return NULL;
+    bool *visited = calloc(GraphNumVertices(g), sizeof(bool));
+
+    dfs(g, src, visited);
+
+    Set s = SetNew();
+
+    for (int w = 0; w < GraphNumVertices(g); w++) {
+        if (visited[w]) SetInsert(s, w);
+    }
+    return s;
 }
